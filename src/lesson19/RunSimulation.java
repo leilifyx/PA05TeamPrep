89b1246416c9a899750b93b7b64974fc72dd29d1
leilifyx <62907855+leilifyx@users.noreply.github.com>
2020-04-09 15:46:08 -0400

    added FrequentFlier parameter to RunSimulation

--- src/lesson19/RunSimulation.java
+++ src/lesson19/RunSimulation.java
@@ -23,7 +23,7 @@ public class RunSimulation {
 		int numStayHome = Integer.parseInt(args[2]);
 		int numEssential = Integer.parseInt(args[3]);
 		int numSkeptic = Integer.parseInt(args[4]);
-		
+		int numFrequentFlier = Integer.parseInt(args[5]);
 
 		// next we create the population and the country
 		Population population;
@@ -33,7 +33,7 @@ public class RunSimulation {
 		//int numEssential = numPeople/10;
 		//int numOther = numPeople/20;
 		//int numStayHome = numPeople - numEssential - numOther;
-		population = new MixedPopulation(numStayHome, numEssential, numSkeptic);
+		population = new MixedPopulation(numStayHome, numEssential, numSkeptic + numFrequentFlier);
 		population.createPeople();
 
 		Country country = new Country(width,height);
